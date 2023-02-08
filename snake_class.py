e7190f5138d61453c2d54000f516dfddf92bfc30
kingdreamerr <jackmvula458@gmail.com>
2023-02-08 23:19:54 +0200

    Add snake class and create the create_snake method

--- snake_class.py
+++ snake_class.py
@@ -4,3 +4,19 @@ UP = 90
 DOWN = 270
 LEFT = 180
 RIGHT = 0
+
+class Snake:
+    def __init__(self):
+        self.snake_house = []
+        self.create_snake()
+        self.head = self.snake_house[0]
+
+    def create_snake(self):
+        x = -20
+        for i in range(0, 3):
+            snake = Turtle("square")
+            snake.penup()
+            snake.color("white")
+            snake.goto(x=x, y=0)
+            x -= 20
+            self.snake_house.append(snake)
