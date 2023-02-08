30c70f462ad6bbfbc8a737f41073ec8eb7dcc501
kingdreamerr <jackmvula458@gmail.com>
2023-02-08 23:26:14 +0200

    Add the onkeypress eventlisteners

--- main.py
+++ main.py
@@ -7,3 +7,13 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake game")
+
+
+screen.tracer(0)
+snake = Snake()
+screen.listen()
+
+screen.onkeypress(snake.up, "Up")
+screen.onkeypress(snake.down,"Down")
+screen.onkeypress(snake.left,"Left")
+screen.onkeypress(snake.right,"Right")
