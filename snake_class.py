a4551bffce8d82e9306fc1366c25ea0b9e36aa22
kingdreamerr <jackmvula458@gmail.com>
2023-02-09 23:01:34 +0200

    Update snake class and add new method to add a square when the snake eats  food

--- snake_class.py
+++ snake_class.py
@@ -1,9 +1,11 @@
 from turtle import Turtle
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE = 20
 UP = 90
 DOWN = 270
 LEFT = 180
 RIGHT = 0
+
 
 class Snake:
     def __init__(self):
@@ -12,15 +14,18 @@ class Snake:
         self.head = self.snake_house[0]
 
     def create_snake(self):
-        x = -20
-        for i in range(0, 3):
-            snake = Turtle("square")
-            snake.penup()
-            snake.color("white")
-            snake.goto(x=x, y=0)
-            x -= 20
-            self.snake_house.append(snake)
+        for position in STARTING_POSITIONS:
+            self.add_square(position)
+          
+    def add_square(self,position):
+        snake = Turtle("square")
+        snake.penup()
+        snake.color("white")
+        snake.goto(position)
+        self.snake_house.append(snake)
 
+    def extend(self):
+        self.add_square(self.snake_house[-1].position())
     def move(self):
         for square in range(len(self.snake_house) - 1, 0, -1):
             new_x = self.snake_house[square - 1].xcor()
