d5cae18073d89ab7f4f38cbb4b143ffe4b9868a2
kingdreamerr <jackmvula458@gmail.com>
2023-02-09 23:02:52 +0200

    Update main and add new instances to the food class and scoreboard class

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from turtle import Screen
 from snake_class import Snake
+from food import Food
+from scoreboard import Board
 import time
-
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -12,17 +13,36 @@ screen.title("Snake game")
 screen.tracer(0)
 snake = Snake()
 screen.listen()
+food = Food()
+board = Board()
+# game_over_text = Board()
 
 screen.onkeypress(snake.up, "Up")
 screen.onkeypress(snake.down,"Down")
 screen.onkeypress(snake.left,"Left")
 screen.onkeypress(snake.right,"Right")
-
+score = 0
 is_end = False
 while not is_end:
     screen.update()
     time.sleep(0.1)
     snake.move()
+    # EAT FOOD 
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        score += 1
+        snake.extend()
+        board.increase_score()
 
+    if snake.head.xcor() > 280 or snake.head.xcor() < -295 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        is_end = True
+        time.sleep(0)
+        board.game_over()
+
+    # Detect collision with the tail.
+    for square in snake.snake_house[1:]:
+        if snake.head.distance(square) < 10:
+            is_end = True
+            board.game_over()
 
 screen.exitonclick()
